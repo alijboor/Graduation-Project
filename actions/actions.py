9b0d1793c1b37c1f27c88b5980d9c23aada238fc
maher_zzz <171010@ppu.edu.ps>
2021-03-24 23:47:53 +0200

    connection with db

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,45 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import mysql.connector
+import traceback
+import pyodbc 
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        try:
+            conn= pyodbc.connect('Driver={SQL Server};'
+            'Server=MaherSalamin\SQLEXPRESS;'
+            'Database=chatbot_db;'
+            'Trusted_Connection=yes;')
+            dispatcher.utter_message("trying to connect")
+            cursor=conn.cursor()
+            #msg= '''SELECT * FROM chatbot_db.dbo.Colleges'''
+            
+            cursor.execute('SELECT * FROM chatbot_db.dbo.Colleges')
+            #dispatcher.utter_message(cursor)
+            dispatcher.utter_message("query should be done1111")
+            for row in cursor:
+                #dispatcher.utter_message([row.id + " " + row.college_name + "\n"])
+                dispatcher.utter_message(text=str(row))
+                print(row)
+        except:
+            dispatcher.utter_message("Failed")
+
+        return []
+
+    #     return []
+
+    #def submit(self,dispatcher,tracker,domain):
+        
