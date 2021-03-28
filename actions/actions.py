b7471fa03b650e7f000308b5104eb7b7919db10d
maher_zzz <171010@ppu.edu.ps>
2021-03-28 13:19:21 +0300

    reconnect with mysql using phpmyadmin

--- actions/actions.py
+++ actions/actions.py
@@ -12,40 +12,31 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import mysql.connector
-import traceback
-import pyodbc 
+
 
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
         return "action_hello_world"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
+    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
-            conn= pyodbc.connect('Driver={SQL Server};'
-            'Server=MaherSalamin\SQLEXPRESS;'
-            'Database=chatbot_db;'
-            'Trusted_Connection=yes;')
-            dispatcher.utter_message("trying to connect")
-            cursor=conn.cursor()
-            #msg= '''SELECT * FROM chatbot_db.dbo.Colleges'''
-            
-            cursor.execute('SELECT * FROM chatbot_db.dbo.Colleges')
-            #dispatcher.utter_message(cursor)
-            dispatcher.utter_message("query should be done1111")
+            conn = mysql.connector.connect(
+            host="localhost",
+            #port= "3306",
+            user="root",
+            password="",
+            database="chatbot_db")
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM colleges")
+            print("now should be executed")
             for row in cursor:
                 #dispatcher.utter_message([row.id + " " + row.college_name + "\n"])
                 dispatcher.utter_message(text=str(row))
-                print(row)
+                print(row) 
+            cursor.close()
+            conn.close() 
         except:
-            dispatcher.utter_message("Failed")
-
+            dispatcher.utter_message(text="Failed!")
         return []
-
-    #     return []
-
-    #def submit(self,dispatcher,tracker,domain):
-        
