ce814f946b857af6a4d8c6d623c2dc895e19f358
Maher Ismael Salamin <171010@ppu.edu.ps>
2021-04-03 14:37:34 +0300

    ask t.branch and t.mark added,formal responses

--- actions/actions.py
+++ actions/actions.py
@@ -97,11 +97,14 @@ class ValidateExpectedMajorForm(FormValidationAction):
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
         if not slot_value:
+            dispatcher.utter_message("أدخل فرع الثانوية ")
             return {
                 "branch_of_tawjihi": None,
             }
         else:
+            # dispatcher.utter_message("أدخل فرع الثانوية ")
             return {"branch_of_tawjihi": slot_value}
 
     def validate_mark_of_branch(
@@ -110,16 +113,20 @@ class ValidateExpectedMajorForm(FormValidationAction):
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
         if not slot_value:
             return {
                 "mark_of_branch": None,
+                
             }
         else:
+            dispatcher.utter_message("أدخل معدلك")
             return {"mark_of_branch": slot_value}
 
 class ActionSubmitExpectedMajor(Action):
     def name(self) -> Text:
-        return "ask_tawjihi_branch_and_mark"
+        return "action_tawjihi_branch_and_mark"
+    
     def run(self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
        
         try:
@@ -132,23 +139,27 @@ class ActionSubmitExpectedMajor(Action):
             branch_tawjihi=tracker.get_slot("branch_of_tawjihi")
             mark_branch=tracker.get_slot("mark_of_branch")
             
-            query="""SELECT major_name FROM majors JOIN tawjihibranch ON tawjihibranch.major_id = majors.id AND tawjihibranch.branch = %s OR tawjihibranch.branch = 'جميع الفروع' WHERE min_gpa <=%s   """
+            query="""SELECT major_name FROM majors 
+            JOIN tawjihibranch ON tawjihibranch.major_id = majors.id 
+            AND tawjihibranch.branch = %s OR tawjihibranch.branch = 'جميع الفروع' 
+            WHERE min_gpa <=%s   """
             mycursor.execute(query,(branch_tawjihi,mark_branch,))
             result= mycursor.fetchall()
+            
             for major in result:
                 fill_slot = '{"major" : "' + re.sub("[(',;)]", "",str(major)) + '"}'
                 Majors_array.append({
                     "title": re.sub("[(',;)]", "", str(major)),
                     "payload": f"/choose_major{fill_slot}"
                 })
-                # Majors_array.append(str(major))
+                
             dispatcher.utter_message(
                 text="التخصصات التي يمكنك التسجيل بها حسب معدلك و فرعك التوجيهي هي التخصصات التالية ", buttons=Majors_array)
             mycursor.close()
             conn.close()
         except mysql.connector.Error as error:
             print("parameterized query failed {}".format(error))
-            dispatcher.utter_message("Failed")
+            dispatcher.utter_message("حدث خطأ أثناء احضار التخصصات")
         return[]
 
 class MajorDeatil(Action):
@@ -164,9 +175,11 @@ class MajorDeatil(Action):
                                            password="",
                                            database="chatbot_db")
             mycursors = conn.cursor()
-            major_need = tracker.get_slot("major")
-            mycursors.execute("""SELECT * FROM majors where major_name = %s""",(major_need, ))
+            major_selected = tracker.get_slot("major")
+            mycursors.execute("""SELECT * FROM majors where major_name = %s""",(major_selected, ))
+            
             result = mycursors.fetchall()
+            print(result)
             final_result = " "
 
             # for d in result:
@@ -177,6 +190,8 @@ class MajorDeatil(Action):
             #         "payload": f"/choose_major{fill_slot}"
             #     })text=f"{final_result}"
             dispatcher.utter_message(json_message = result)
+            
+
             mycursors.close()
             conn.close()
 
