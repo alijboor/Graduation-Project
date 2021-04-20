67e9080ecd60f97b9b22f6b3a841184d5cc66168
Ali Jboor <171001@ppu.edu.ps>
2021-04-20 15:23:36 +0300

    alot of changes and add new features

--- actions/actions.py
+++ actions/actions.py
@@ -1,9 +1,10 @@
 from typing import Any, Text, Dict, List
-from rasa_sdk.events import AllSlotsReset, SlotSet, EventType
+from rasa_sdk.events import AllSlotsReset, SlotSet, EventType, UserUtteranceReverted, SessionStarted, ActionExecuted
 #from rasa_sdk.types import DomainDict
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
+
 
 import mysql.connector
 import re
@@ -11,7 +12,15 @@ import re
 Colleges_array = []
 Majors_array = []
 
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(Text="اهلا و سهلا في مساعد التسجيل لــ جامعـة بوليتكنك فلسـطـيـن")
+        return [SessionStarted(), ActionExecuted("action_listen")]
 
+        
 class ActionColleges(Action):
     def name(self) -> Text:
         return "action_colleges"
@@ -110,11 +119,13 @@ class ValidateExpectedMajorForm(FormValidationAction):
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        slot_value = int(tracker.get_slot("mark_of_branch"))
+        slot_value = float(tracker.get_slot("mark_of_branch"))
         if slot_value < 50 :
-            if slot_value > 100 :
-                dispatcher.utter_message(text=" أعدإدخال معدلك بشكل صحيح رجاءً")
-                return {"mark_of_branch": None,}
+            dispatcher.utter_message(text=" أعد إدخال معدلك بشكل صحيح رجاءً")
+            return {"mark_of_branch": None}
+        elif slot_value > 100 :
+            dispatcher.utter_message(text=" أعد إدخال معدلك بشكل صحيح رجاءً")
+            return {"mark_of_branch": None}
         else:
             return {"mark_of_branch": slot_value}
 
@@ -148,7 +159,7 @@ class ActionSubmitExpectedMajor(Action):
             mark_branch=tracker.get_slot("mark_of_branch")
             prog=tracker.get_slot("program")
             
-            query="""SELECT major_name FROM majors JOIN tawjihibranch ON tawjihibranch.major_id = majors.id AND (tawjihibranch.branch = %s OR tawjihibranch.branch = 'جميع الفروع') WHERE min_gpa <=%s  AND program = %s """
+            query="""SELECT major_name FROM majors JOIN tawjihibranch ON tawjihibranch.major_id = majors.id AND (tawjihibranch.branch = %s OR tawjihibranch.branch = 'جميع الفروع' ) WHERE min_gpa <=%s  AND program = %s """
             mycursor.execute(query,(branch_tawjihi,mark_branch,prog,))
             result= mycursor.fetchall()
             if result:
@@ -158,7 +169,7 @@ class ActionSubmitExpectedMajor(Action):
                         "title": re.sub("[(',;)]", "", str(major)),
                         "payload": f"/choose_major{fill_slot}"
                     })
-                dispatcher.utter_message(text="التخصصات التي يمكنك التسجيل بها حسب معدلك و فرعك التوجيهي هي التخصصات التالية ", buttons=Majors_array)
+                dispatcher.utter_message(text=" التخصصات التي يمكنك التسجيل بها حسب معدلك و فرعك التوجيهي هي التخصصات التالية:\n ملاحظة: حصول الطالب على الحد الأدنى المطلوب لمعدل الثانوية لا يني بالضرورة القبول في التخصص المطلوب و إنما يهضع الطالب للتنافس مع الطلبة الراغبين في الالتحاق بأي تخصص ", buttons=Majors_array)
             else:
                 dispatcher.utter_message(text=f"نأسف لعدم استيفائك شروط القبول لاي من التخصصات التابعة لبرنامج {prog} ")
 
@@ -184,8 +195,8 @@ class MajorDeatil(Action):
             mycursors = conn.cursor()
             major_selected = tracker.get_slot("major")
             mycursors.execute("""SELECT * FROM majors where major_name = %s""",(major_selected, ))
-            result = mycursors.fetchall()
-            dispatcher.utter_message(json_message = result)
+            result = list(mycursors.fetchall())
+            dispatcher.utter_message(text = f"تخصص {result[0][2]} : يبلغ سعر الساعة لهذا التخصص {result[0][3]} ديناراً و بإجمالي {result[0][4]} ساعة موزعة على {result[0][5]} سنوات, و يبلغ الحد الادنى لمعدلات القبول لهذا التخصص {result[0][6]} في فروع الثانوية التالية : علمي و صناعي")
             mycursors.close()
             conn.close()
         except mysql.connector.Error as error:
